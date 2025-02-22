63c2d95272c0444b3a8aba13f633674cc223d732
Fuhu0411 <126298001+Fuhu0411@users.noreply.github.com>
2025-02-21 18:19:13 -0600

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import crawler
 import network_security_HTTPS
 import network_security_Encryption_Protocol
+import network_security_SSL
 
 def main_file():
     """Ask user for a website URL and check if HTTPS is enabled"""
@@ -11,9 +12,19 @@ def main_file():
     https_links =crawler.extract_external_links(url)
 
 
+    if len(https_links)>0:
+        print("\n🔗 External HTTPS Links Found:")
+        for link in https_links:
+            print(link)
 
+    print(f"\n\n--CHECKING TLS CERTIFICATES--\n")
     #Checking https_links for TLS encryption
     network_security_Encryption_Protocol.check_encryption_protocol(https_links)
+
+
+    print(f"\n\n--CHECKING SSL CERTIFICATES--\n")
+    #Checking https_links for SSL certificates
+    network_security_SSL.check_encryption_protocol_for_hosts(https_links)
 
     #network_security_HTTPS.check_https(found_links)
 
